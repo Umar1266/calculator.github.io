182f4f915f4103cfe41f4fb7d96426ccc19935c7
Umar1266 <ansariumar12355@gmail.com>
2024-07-15 22:43:24 +0530

    just uploaded a code of calculator 15-07, 10:45

---
+++ script.js
@@ -0,0 +1,24 @@
+function c(button){
+    // document.querySelector("#cal"+button).innerHTML="(";
+    // document.querySelector("#cal"+button).innerHTML=")";
+    // document.querySelector("#cal"+button).innerHTML="%";
+    // document.querySelector("#cal"+button).innerHTML="/";
+    // document.querySelector("#cal"+button).innerHTML="7";
+    // document.querySelector("#cal"+button).innerHTML="8";
+    // document.querySelector("#cal"+button).innerHTML="9";
+    // document.querySelector("#cal"+button).innerHTML="x";
+    // document.querySelector("#cal"+button).innerHTML="4";
+    // document.querySelector("#cal"+button).innerHTML="5";
+    // document.querySelector("#cal"+button).innerHTML="6";
+    // document.querySelector("#cal"+button).innerHTML="-";
+    // document.querySelector("#cal"+button).innerHTML="1";
+    // document.querySelector("#cal"+button).innerHTML="2";
+    // document.querySelector("#cal"+button).innerHTML="3";
+    // document.querySelector("#cal"+button).innerHTML="+";
+    // document.querySelector("#cal"+button).innerHTML="";
+    // document.querySelector("#cal"+button).innerHTML="0";
+    // document.querySelector("#cal"+button).innerHTML=".";
+    // document.querySelector("#cal"+button).innerHTML="";
+
+    document.querySelector("#cal").innerHTML=button;
+}
